79d171cecfdf92b9d3f1ca856c223e4c6ba55471
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2022-07-20 20:12:40 +0400

    Поправил поднятие объявлений Rosrealt

--- classes/Sites/Rosrealt.js
+++ classes/Sites/Rosrealt.js
@@ -1,7 +1,6 @@
 let util = require('util');
 let Site = require('./Site');
 
-
 /**
  * Функция-конструктор для работы с Rosrealt
  *
@@ -11,9 +10,9 @@ let Site = require('./Site');
  */
 let Rosrealt = function Rosrealt (settings) {
     Site.call(this, settings);
-    this.mainPageURL = 'https://rosrealt.ru';
-    this.logoutPageURL = this.mainPageURL + '/logout';
-    this.adsPageURL =  this.mainPageURL + '/kabinet';
+    this.mainPageURL     = 'https://rosrealt.ru';
+    this.logoutPageURL   = this.mainPageURL + '/logout';
+    this.adsPageURL      = this.mainPageURL + '/kabinet';
     this.authFormPageURL = this.mainPageURL + '/authorization';
 };
 
@@ -28,28 +27,23 @@ util.inherits(Rosrealt, Site); // наследование
  */
 Rosrealt.prototype.authorize = async function (oAccountLoginData) {
     let page = await this.getPage();
-
     await page.goto(this.mainPageURL);
     await page.evaluate(async (oAccountLoginData, authFormPageURL) => {
         let formData = new FormData();
         formData.append('Email', oAccountLoginData.login);
         formData.append('Password', oAccountLoginData.password);
-        await fetch(authFormPageURL,{
+        await fetch(authFormPageURL, {
             method: 'POST',
-            body: formData,
+            body:   formData
         });
     }, oAccountLoginData, this.authFormPageURL);
     await page.goto(this.mainPageURL, {waitUntil: 'domcontentloaded'});
-    /** Коллекция со всеми кнопками Поднять типа JSHandle*/
-    let toProfileLinks  = await page.$$('a.user-menu__link_sign-in');
+    /** Коллекция со всеми кнопками Перехода в ЛК JSHandle*/
+    let toProfileLinks = await page.$$('a.user-menu__link_sign-in');
     if (toProfileLinks.length) {
-        await toProfileLinks[toProfileLinks.length-1].click();
-    }
-    if (!page.url().includes(this.adsPageURL)) { // Если после всего мы не находимся на стрнице объявлений
-        throw new Error('При авторизации произошла ошибка');
+        await toProfileLinks[toProfileLinks.length - 1].click();
     }
 };
-
 
 /**
  * Обновить объявления пользователя с логином sLogin и паролем sPassword
@@ -59,11 +53,14 @@ Rosrealt.prototype.authorize = async function (oAccountLoginData) {
 Rosrealt.prototype.updateAds = async function (oAccountLoginData) {
     let page = await this.getPage();
     await this.authorize(oAccountLoginData);
-    /** Коллекция со всеми кнопками поднятия типа JSHandle*/
-    let arAllBtns  = await page.$$('.p_options input[name="actualize"]');
-    if (arAllBtns.length) {
-        await arAllBtns[arAllBtns.length-1].click();
-    }
+    await page.waitForSelector('a.catalog-card__button_promote', {visible: true});
+    await page.evaluate(async () => {
+        /** Коллекция со всеми кнопками поднятия типа JSHandle*/
+        var allButtons = document.querySelectorAll('a.catalog-card__button_promote');
+        for (var i = 0; i < allButtons.length; i++) {
+            await fetch(allButtons[i].href);
+        }
+    });
 };
 
 module.exports = Rosrealt;
